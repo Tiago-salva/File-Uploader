3e8c130bd4aee0d1e331aeae7fb6e4e975d2c58d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-14 17:43:26 -0300

    feat: implement passport authentication with Prisma integration

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,2 +1,43 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+function initializePassport(passport) {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        try {
+          const user = await prisma.user.findUnique({ where: { email } });
+          console.log(user);
+          if (!user) {
+            return done(null, false, { message: "User not found" });
+          }
+
+          const match = await bcrypt.compare(password, user.password);
+          if (!match) {
+            return done(null, false, { message: "Incorrect password" });
+          }
+
+          return done(null, user);
+        } catch (err) {
+          return done(err);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { id } });
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
+
+module.exports = initializePassport;

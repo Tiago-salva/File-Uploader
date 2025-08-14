fe005426a3b053b85249212e5530cf3a801c2d50
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-14 17:36:33 -0300

    feat: refactor session management to use PrismaSessionStore

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const path = require("node:path");
 require("dotenv").config();
 
 const express = require("express");
-const session = require("express-session");
+const expressSession = require("express-session");
 const passport = require("passport");
-const pgSession = require("connect-pg-simple")(session);
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 
-const pool = require("./config/db-config");
+// Custom configuration
 const initializePassport = require("./config/passport-config");
 
 // Import routers
+const userRouter = require("./src/routes/userRouter");
 
+// App
 const app = express();
 
 app.set("views", path.join(__dirname, "src", "views"));
@@ -21,8 +24,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 // Passport
+initializePassport(passport);
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.session());
+
+// Gives acces to currentUser to all the views
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // Use routes
+app.use("/", userRouter);
+app.get("/", (req, res) => res.render("home"));
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`app listening to port ${process.env.PORT || 3000}`)

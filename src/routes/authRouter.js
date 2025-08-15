7f34d9bf0183b72cf281711489d7a723d494da24
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-15 20:38:21 -0300

    feat: implement authRouter for handling authentication routes

---
+++ src/routes/authRouter.js
@@ -0,0 +1,31 @@
+const {
+  renderLogInForm,
+  createUserGet,
+  createUserPost,
+  logOut,
+} = require("../controllers/authController");
+const passport = require("passport");
+const { Router } = require("express");
+const { isNotAuthenticated } = require("../middleware/authMiddleware");
+const authRouter = Router();
+
+// Log in
+authRouter.get("/log-in", isNotAuthenticated, renderLogInForm);
+
+authRouter.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+  })
+);
+
+// Sign up
+authRouter.get("/sign-up", isNotAuthenticated, createUserGet);
+
+authRouter.post("/sign-up", createUserPost);
+
+// Log out
+authRouter.get("/log-out", logOut);
+
+module.exports = authRouter;

fc2694138447ef196e2ed3568aae26f9100c77ae
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-15 20:37:57 -0300

    feat: implement authController for handling authentication routes

---
+++ src/controllers/authController.js
@@ -0,0 +1,31 @@
+const db = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
+// Log in
+async function renderLogInForm(req, res) {
+  res.render("log-in-form");
+}
+
+// Sign up
+async function createUserGet(req, res) {
+  res.render("sign-up-form");
+}
+
+async function createUserPost(req, res) {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  await db.addUser(req.body, hashedPassword);
+  res.redirect("/");
+}
+
+// Log out
+async function logOut(req, res) {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.redirect("/log-in");
+  });
+}
+
+module.exports = { renderLogInForm, createUserGet, createUserPost, logOut };

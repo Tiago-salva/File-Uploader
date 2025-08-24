e063edd0ccfee0c84df4d3e2b46bd20c099a76b5
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-24 18:21:51 -0300

    feat: when uploading a file, it is linked to the selected folder

--- src/routes/fileRouter.js
+++ src/routes/fileRouter.js
@@ -2,31 +2,32 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const { Router } = require("express");
 const upload = require("../../config/multer-config");
+const { renderUploadForm } = require("../controllers/fileController");
 const fileRouter = Router();
 
-fileRouter.get("/", (req, res) => res.render("upload-form"));
+fileRouter.get("/", renderUploadForm);
 
 fileRouter.post("/", upload.single("myFile"), async (req, res) => {
   try {
-    await prisma.folder.create({
-      data: {
-        name: "main",
-        userId: 6,
-      },
-    });
-    // const { folderId } = req.body;
+    let targetFolderId = parseInt(req.body.folderId);
+    // If there is no folder selected use root folder
+    if (!targetFolderId) {
+      const userId = req.user.id;
+      const rootFolder = await prisma.folder.findFirst({
+        where: { userId: userId, name: "Root" },
+      });
+      targetFolderId = rootFolder.id;
+    }
 
-    // Cloudinary devuelve URL en req.file.path
     const file = await prisma.file.create({
       data: {
         name: req.file.originalname,
         size: req.file.size,
-        url: req.file.path, // <- el URL en la nube
-        folderId: 1,
+        url: req.file.path,
+        folderId: targetFolderId,
       },
     });
-    console.log(req.file);
-    res.json(file);
+    res.redirect("/");
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Error subiendo archivo" });

0acd9cc01304f1e59aba2f05d1a341e366d3dcc3
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-24 18:19:19 -0300

    feat: when creating a user, a root folder is created

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,13 +2,22 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function addUser(data, hashedPassword) {
-  return await prisma.user.create({
+  const newUser = await prisma.user.create({
     data: {
       name: data.name,
       email: data.email,
       password: hashedPassword,
     },
   });
+
+  const rootFolder = await prisma.folder.create({
+    data: {
+      name: "Root",
+      userId: newUser.id,
+    },
+  });
+
+  return { newUser, rootFolder };
 }
 
 module.exports = { addUser };

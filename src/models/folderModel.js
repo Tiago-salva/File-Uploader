efb32e763732fd4596019291205c48540b461002
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-08-27 10:05:36 -0300

    feat: add getHomeData to retieve all the folders and the root files

--- src/models/folderModel.js
+++ src/models/folderModel.js
@@ -10,12 +10,30 @@ async function addFolder(name, userId) {
   });
 }
 
+async function getHomeData(userId) {
+  const rootFolder = await prisma.folder.findFirst({
+    where: { userId: parseInt(userId), name: "Root" },
+  });
+
+  if (!rootFolder) throw new Error("Root folder not found");
+
+  const folders = await prisma.folder.findMany({
+    where: {
+      userId: parseInt(userId),
+      NOT: { id: rootFolder.id },
+    },
+  });
+
+  const rootFiles = await prisma.file.findMany({
+    where: { folderId: rootFolder.id },
+  });
+
+  return { folders, rootFiles };
+}
+
 async function getAllFolders(userId) {
   return await prisma.folder.findMany({
-    where: { userId: parseInt(userId) },
-    include: {
-      files: true,
-    },
+    where: { userId: userId },
   });
 }
 
@@ -28,8 +46,22 @@ async function getFolder(id) {
   });
 }
 
+async function deleteFolder(id) {
+  await prisma.file.deleteMany({
+    where: { folderId: id },
+  });
+
+  await prisma.folder.delete({
+    where: {
+      id: id,
+    },
+  });
+}
+
 module.exports = {
   addFolder,
+  getHomeData,
   getAllFolders,
   getFolder,
+  deleteFolder,
 };
